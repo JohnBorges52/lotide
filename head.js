36f294107cf4584ac485d8e76ec65faaa85e2f82
John <jrborges52@email.com>
2022-05-31 04:09:54 +0000

    added a new function

---
+++ head.js
@@ -0,0 +1,31 @@
+const head = function(argument) {
+  if (argument !== undefined){
+  return argument[0]
+} else {
+  return undefined
+}
+
+}
+
+const assertEqual = function(actual, expected) {
+  
+  if (actual  === expected) {
+    console.log(`✅  ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ${actual} !== ${expected}`);
+  }
+ 
+  
+
+
+
+};
+
+
+
+
+
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
